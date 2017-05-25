d1716100aa5a0910998643a321a0e44387481cdd
eleven <eleven@ling>
2017-05-25 20:57:32 +0800

    Signed-off-by: eleven <eleven@ling>

---
+++ src/test/java/cn/codehero/junit/CalculatorTest.java
@@ -0,0 +1,10 @@
+package cn.codehero.junit;
+
+public class CalculatorTest {
+		@Test
+		public void testAdd(){
+			Calculator calculator = new Calculator();
+			double result = calculator.add(10, 50);
+			assertEquals{60,result,0};
+		}
+}
